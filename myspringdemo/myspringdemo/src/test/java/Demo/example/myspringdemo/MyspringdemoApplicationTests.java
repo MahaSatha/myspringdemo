dec7bd1b05687476031c3ecd0aedcb0f7d817685
MahaSatha <81618394+MahaSatha@users.noreply.github.com>
2021-03-31 10:02:55 +0530

    Add files via upload

---
+++ myspringdemo/myspringdemo/src/test/java/Demo/example/myspringdemo/MyspringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Demo.example.myspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
